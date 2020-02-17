514d921aa31589fc3a2e8e1514692aee4dde8cdc
aqurink <junkuwieazy@gmail.com>
2020-02-17 12:56:40 +0700

    remove some file

--- index.php
+++ index.php
@@ -1,23 +1,74 @@
 <?php
 
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-use React\Socket\ConnectionInterface;
-use React\EventLoop\Factory as ReactLoop;
-use React\Socket\Server as ReactSocket;
+//Ratchet
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use Ratchet\ConnectionInterface;
+use Ratchet\MessageComponentInterface;
 
-// React Mysql
-use React\MySQL\Factory as MysqlFactory;
-use React\MySQL\QueryResult;
-
-$loop = ReactLoop::create();
-$socket = new ReactSocket(8089, $loop);
+// DB
+use voku\db\DB;
 
 
-class Socket {
+class Socket implements MessageComponentInterface {
     
+    protected $clients;
+    private $subscriptions;
+    private $users;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+        $this->subscriptions = [];
+        $this->users = [];
+        echo "__construct \n";
+    }
+
+    ////// OPEN
+    public function onOpen(ConnectionInterface $interface) {
+        // STORE NEW CLIENT TO WS
+        // STORE NEW CLIENT
+        $this->users[$interface->resourceId] = $interface;
+        echo "Open \n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        //Async Mysql
+        $db = DB::getInstance('localhost', 'root', '', 'wktpanel_db');
+        $result = $db->query("SELECT * FROM kategori_tb WHERE id = ".$msg." LIMIT 1");
+       
+        $dataUsers = array();
+        $users  = $result->fetchAll();
+        foreach($users as $key => $val) {
+            $dataUsers[$key] = $val;
+        }
+
+        print_r(json_encode($dataUsers)."\n");
+
+        $this->users[$from->resourceId]->send(json_encode($dataUsers));
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        echo "Close \n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "$e \n";
+        $conn->close();
+    }
 }
 
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Socket()
+        )
+    ),
+    8085
+);
 
-
-$loop->run();
+echo "Socket On 8085 \n";
+$server->run();
