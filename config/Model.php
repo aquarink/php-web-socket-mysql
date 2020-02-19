8a7a23d4765625c1f49f958bb79a273e7b2a2b43
aqurink <junkuwieazy@gmail.com>
2020-02-19 16:07:00 +0700

    Perbaikan flow dan model
    Perbaikan flow dan model

--- config/Model.php
+++ config/Model.php
@@ -3,15 +3,21 @@ use voku\db\DB;
 
 class Model {
     
-    protected $database;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
 
     public function __construct()
     {
-        echo "DB Ready \n";
+        $this->host         = 'localhost';
+        $this->username     = 'root';
+        $this->password     = '';
+        $this->database     = 'momoka_db';
     }
 
     public function insert_logWsClients($id_ws, $user_token, $game_id) {
-        $db = DB::getInstance('localhost', 'root', '', 'momoka_db');
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
 
         $data = [
             'id_ws'         => $id_ws,
@@ -29,32 +35,53 @@ class Model {
     }
 
     public function check_userLog($id_ws) {
-        $db = DB::getInstance('localhost', 'root', '', 'momoka_db');
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
 
-        $query = $db->query("SELECT id, game_id FROM log_ws_clients WHERE id_ws = '".$id_ws."' LIMIT 1");
-        $user  = $query->fetchAll();
-        if(count($user) > 0) {
-            return $user;
+        $query = $db->query("SELECT id, game_id, user_token FROM log_ws_clients WHERE id_ws = '".$id_ws."' LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
         } else {
             return array();
         }
     }
 
-    public function get_roomUsers($game_id) {
-        $db = DB::getInstance('localhost', 'root', '', 'momoka_db');
+    public function get_roomData($game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
 
-        $query = $db->query("SELECT lws.id, lws.id_ws, usr.name FROM log_ws_clients lws INNER JOIN user usr ON usr.token = lws.user_token WHERE lws.game_id = '".$game_id."' AND lws.id_ws != 0");
-        $user  = $query->fetchAll();
-        if(count($user) > 0) {
-            return $user;
+        $query = $db->query("SELECT id_ws, user_token FROM log_ws_clients WHERE game_id = '".$game_id."' AND id_ws <> 0");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
         } else {
             return array();
+        }
+    }
+
+    public function reset_LogWs() {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $where = [
+            'id_ws <> ' => 0,
+            'updated_at IS ' => NULL,
+        ];
+
+        $update = [
+            'id_ws'         => 0,
+            'updated_at'    => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->update('log_ws_clients', $update, $where);
+        if($query) {
+            return true;
+        } else {
+            return false;
         }
     }
 
     public function update_LogWs($id, $id_ws)
     {
-        $db = DB::getInstance('localhost', 'root', '', 'momoka_db');
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
 
         $where = [
             'id'    => $id,
@@ -75,14 +102,229 @@ class Model {
     }
 
     public function check_user($user_token) {
-        $db = DB::getInstance('localhost', 'root', '', 'momoka_db');
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
 
-        $query = $db->query("SELECT id FROM user WHERE token = '".$user_token."' LIMIT 1");
-        $user  = $query->fetchAll();
-        if(count($user) > 0) {
-            return $user;
+        $query = $db->query("SELECT * FROM user WHERE token = '".$user_token."' LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
         } else {
             return array();
+        }
+    }
+
+    public function check_userIdWs($id_ws, $game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $query = $db->query("SELECT * FROM log_ws_clients WHERE id_ws = '".$id_ws."' AND game_id = '".$game_id."' LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
+        } else {
+            return array();
+        }
+    }
+
+    public function check_game($game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $query = $db->query("SELECT id FROM game WHERE id = '".$game_id."' LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
+        } else {
+            return array();
+        }
+    }
+
+    public function check_playing($game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $query = $db->query("SELECT * FROM log_ws_play WHERE game_id = '".$game_id."' AND is_play = 1 LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
+        } else {
+            return array();
+        }
+    }
+
+    public function insert_logWsChats($id_ws, $user_token, $game_id, $chat) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $data = [
+            'id_ws'         => $id_ws,
+            'user_token'    => $user_token,
+            'game_id'       => $game_id,
+            'chat'          => $chat,
+            'created_at'    => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->insert('log_ws_chats', $data);
+        if($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function insert_logWsPlay($id_ws, $user_token, $game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $data = [
+            'id_ws'         => $id_ws,
+            'user_token'    => $user_token,
+            'game_id'       => $game_id,
+            'is_play'       => 1,
+            'start_play'    => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->insert('log_ws_play', $data);
+        if($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function end_logWsPlay($id_ws, $user_token, $game_id)
+    {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $where = [
+            'id_ws'         => $id_ws,
+            'user_token'    => $user_token,
+            'game_id'       => $game_id,
+        ];
+
+        $update = [
+            'is_play'       => 0,
+            'end_play'      => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->update('log_ws_play', $update, $where);
+        if($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function reset_LogWsPlay()
+    {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $where = [
+            'end_play IS ' => NULL,
+        ];
+
+        $update = [
+            'is_play'     => 0,
+            'end_play'    => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->update('log_ws_play', $update, $where);
+        if($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function check_on_playing($id_ws, $user_token, $game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $query = $db->query("SELECT * FROM log_ws_play WHERE id_ws = '".$id_ws."' AND user_token = '".$user_token."' AND game_id = '".$game_id."' AND is_play = 1 LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
+        } else {
+            return array();
+        }
+    }
+
+    public function check_on_playing_idWs($id_ws) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $query = $db->query("SELECT * FROM log_ws_play WHERE id_ws = '".$id_ws."' AND is_play = 1 LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
+        } else {
+            return array();
+        }
+    }
+
+    public function end_outLogWsPlay($id_ws, $user_token, $game_id)
+    {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $where = [
+            'id_ws'         => $id_ws,
+            'user_token'    => $user_token,
+            'game_id'       => $game_id,
+        ];
+
+        $update = [
+            'is_play'     => 0,
+            'end_play'    => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->update('log_ws_play', $update, $where);
+        if($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function check_logWsMesin($game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $query = $db->query("SELECT * FROM log_ws_mesin WHERE id_ws <> 0 AND game_id = '".$game_id."' AND refused_at IS NULL LIMIT 1");
+        $data  = $query->fetchAll();
+        if(count($data) > 0) {
+            return $data;
+        } else {
+            return array();
+        }
+    }
+
+    public function insert_logWsMesin($id_ws, $game_id) {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $data = [
+            'id_ws'         => $id_ws,
+            'game_id'       => $game_id,
+            'connected_at'  => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->insert('log_ws_mesin', $data);
+        if($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function reset_LogWsMesin()
+    {
+        $db = DB::getInstance($this->host, $this->username, $this->password, $this->database);
+
+        $where = [
+            'refused_at IS ' => NULL,
+        ];
+
+        $update = [
+            'id_ws'         => 0,
+            'refused_at'    => date('Y-m-d H:i:s'),
+        ];
+
+        $query = $db->update('log_ws_mesin', $update, $where);
+        if($query) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- index.php
+++ index.php
@@ -18,7 +18,18 @@ class Socket implements MessageComponentInterface {
 
     public function __construct()
     {
+        $model = new Model();
         echo "Engine Ready\n";
+
+        
+        $model->reset_LogWs();
+        echo "Reset Log Client\n";
+
+        $model->reset_LogWsPlay();
+        echo "Reset Log Playing\n";
+
+        $model->reset_LogWsMesin();
+        echo "Reset Log Playing\n";
     }
 
     ////// OPEN
@@ -39,38 +50,82 @@ class Socket implements MessageComponentInterface {
         } else {
             $qs = explode('&', $get_qs);
             if(count($qs) == 2) {
-                $data_qs = array(
-                    'game_id'       => $qs[0],
-                    'user_token'    => $qs[1],
-                );
-
+                // $qs[0] // game_id,
+                // $qs[1] // user_token,
                 
+                // CHECK GAME
+                $check_game = $model->check_game($qs[0]);
+                if(count($check_game) > 0) {
+                    // CHECK USER TOKEN
+                    $check_user_token = $model->check_user($qs[1]);
+                    if(count($check_user_token) > 0) {
+                        $exec = $model->insert_logWsClients($interface->resourceId, $qs[1], $qs[0]);
+                        if($exec) {
+                            $button = false;
+                            $check_playing = $model->check_playing($qs[0]);
+                            if(count($check_playing) == 0) {
+                                $button = true;
+                            } else {
+                                foreach ($check_playing as $keyCplay => $valCplay) {
+                                    if($valCplay->id_ws == $interface->resourceId) {
+                                        $button = true;
+                                    }
+                                }
+                            }
 
-                // CHECK TOKEN
-                $check_token = $model->check_user($qs[1]);
-                if(count($check_token) > 0) {
-                    $exec = $model->insert_logWsClients($interface->resourceId, $qs[1], $qs[0]);
-                    if($exec) {
-                        $send = array(
-                            'code'  => 200,
-                            'datas' => array(
-                                'id'    => $interface->resourceId,
-                            ),
-                            'msg'   => 'You are connected'
-                        );
+                            $send = array(
+                                'button_play'   => $button,
+                            );
+
+                            // SELF
+                            $this->ws_client[$interface->resourceId]->send(json_encode($send));
+
+
+                            // ROOM DATA
+                            $room_data = $model->get_roomData($qs[0]);
+                            if(count($room_data) > 0) {
+                                $new_user_data = array();
+                                foreach ($room_data as $keyRoom => $valRoom) {
+                                    foreach ($check_user_token as $keyUser => $valUser) {
+                                        $broadcast = array(
+                                            'watch' => array(
+                                                'name'   => $valUser->name,
+                                                'avatar' => 'https://www.svgrepo.com/show/160232/avatar.svg?si='.$valUser->name,
+                                            ),
+                                        );
+
+                                        $this->ws_client[$valRoom->id_ws]->send(json_encode($broadcast));
+                                    }
+                                }
+                            } else {
+                                echo "No one in room ".$qs[0]."\n";
+                            }
+                        } else {
+                            $send = array(
+                                'code'  => 404,
+                                'datas' => array(),
+                                'msg'   => 'Save logs failed'
+                            );
+
+                            $this->ws_client[$interface->resourceId]->send(json_encode($send));
+                        }
                     } else {
                         $send = array(
                             'code'  => 404,
                             'datas' => array(),
-                            'msg'   => 'Save logs failed'
+                            'msg'   => 'User not found'
                         );
+
+                        $this->ws_client[$interface->resourceId]->send(json_encode($send));
                     }
                 } else {
                     $send = array(
                         'code'  => 404,
                         'datas' => array(),
-                        'msg'   => 'User not found'
+                        'msg'   => 'Game not found'
                     );
+
+                    $this->ws_client[$interface->resourceId]->send(json_encode($send));
                 }
             } else {
                 $send = array(
@@ -78,25 +133,173 @@ class Socket implements MessageComponentInterface {
                     'datas' => array(),
                     'msg'   => 'Invalid parameters'
                 );
+
+                $this->ws_client[$interface->resourceId]->send(json_encode($send));
+            }
+        }
+    }
+
+    public function onMessage(ConnectionInterface $interface, $message) {
+        $model = new Model();
+
+        $request = json_decode($message);
+        if(json_last_error() === JSON_ERROR_NONE) {
+            foreach ($request as $action => $data) {
+                if($action == 'chat') {
+
+                    $interface_user_data = $model->check_userIdWs($interface->resourceId, $data->room);
+                    if(count($interface_user_data) > 0) {
+                        foreach ($interface_user_data as $keyIface => $valIface) {
+
+                            $game_id    = $data->room;
+                            $mssg       = $data->mssg;
+
+                            $insert_chat_log = $model->insert_logWsChats($interface->resourceId, $valIface->user_token, $game_id, $mssg);
+
+                            // pusg config
+                            $list_user_in_room = $model->get_roomData($game_id);
+                            foreach ($list_user_in_room as $keyRoom => $valRoom) {
+
+                                $user_token = $model->check_user($valRoom->user_token);
+                                if(count($user_token) > 0) {
+                                    foreach ($user_token as $keyUsr => $valUsr) {
+                                        $broadcast = array(
+                                            'chat'  => array(
+                                                'yes'   => true,
+                                                'from'  => $valUsr->name,
+                                                'mssg'  => $mssg,
+                                            ),
+                                        );
+
+                                        $this->ws_client[$valRoom->id_ws]->send(json_encode($broadcast));
+                                    }
+                                } else {
+                                    echo "User not found 1 \n";
+                                }
+                            }
+                        }
+                    } else {
+                        echo "string 1\n";
+                    }
+                } elseif($action == 'play') {
+
+                    $interface_user_data = $model->check_userIdWs($interface->resourceId, $data->room);
+                    if(count($interface_user_data) > 0) {
+                        foreach ($interface_user_data as $keyIface => $valIface) {
+
+                            $game_id    = $data->room;
+
+                            $check_playing = $model->check_playing($game_id);
+                            if(count($check_playing) > 0) {
+                                $button = false;
+                                foreach ($check_playing as $keyCplay => $valCplay) {
+                                    if($valCplay->id_ws == $interface->resourceId) {
+                                        $button = true;
+                                    }
+                                }
+
+                                $send = array(
+                                    'button_play'   => $button,
+                                );
+
+                                // SELF
+                                $this->ws_client[$interface->resourceId]->send(json_encode($send));
+                            } else {
+                                $insert_play_log = $model->insert_logWsPlay($interface->resourceId, $valIface->user_token, $game_id);
+
+                                // pusg config
+                                $list_user_in_room = $model->get_roomData($game_id);
+
+                                $send = array(
+                                    'button_play'   => true,
+                                );
+
+                                // SELF
+                                $this->ws_client[$interface->resourceId]->send(json_encode($send));
+
+
+                                foreach ($list_user_in_room as $keyRoom => $valRoom) {
+
+                                    $user_token = $model->check_user($valRoom->user_token);
+                                    if(count($user_token) > 0) {
+
+                                        foreach ($user_token as $keyUsr => $valUsr) {
+                                            if($valRoom->id_ws != $interface->resourceId) {
+                                                $broadcast = array(
+                                                    'button_play'   => false,
+                                                );
+
+                                                // BROADCAST
+                                                $this->ws_client[$valRoom->id_ws]->send(json_encode($broadcast));
+                                            }
+                                        }
+                                    } else {
+                                        echo "User not found 2 \n";
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        echo "string 2\n";
+                    }
+
+                } elseif($action == 'control') {
+                    $game_id    = 1;
+                    // $game_id    = $data->room;
+                    $cmd       = "broadcast: ".$data->cmd;
+
+                    $check_comming = $model->check_logWsMesin($game_id);
+                    if(count($check_comming) > 0) {
+
+                        foreach ($check_comming as $keyMsn => $valMsn) {
+                            $id_ws_mesin = $valMsn->id_ws;
+
+                            // PERINTAH KE MESIN
+                            $this->ws_client[$id_ws_mesin]->send(json_encode($cmd));
+                        }
+
+                    } else {
+                        echo "Mesin tidak ada \n";
+                    }
+                } else {
+                    echo "string ujung \n";
+                }
+            } // end foreach
+        } else {
+            // Perintah iregular untuk user, harusnya untuk mesin komunikasi
+            // echo "Komunikasi mesin";
+            if($message == "*1234*mulai*") {
+                // CHECK ID MESIN
+
+                // MASUKAN KEDALAM LOG
+                // CONTOH 1234 = 1
+                $game_id = 1;
+                $check_comming = $model->check_logWsMesin($game_id);
+                if(count($check_comming) == 0) {
+                    $model->insert_logWsMesin($interface->resourceId, $game_id);
+                }
+            } elseif($message == "*1234*x*0") {
+
+                $check_user_play = $model->check_on_playing_idWs($interface->resourceId);
+                if(count($check_user_play) > 0) {
+                    foreach ($check_user_play as $keyCUP => $valCUP) {
+                        $model->end_outLogWsPlay($interface->resourceId, $valCUP->user_token, $valCUP->game_id);
+
+                        $send = array(
+                            'button_play'   => false,
+                        );
+
+                        // SELF
+                        $this->ws_client[$interface->resourceId]->send(json_encode($send));
+                    }
+                } else {
+                    echo "check_user_play \n";
+                }
+            } else {
+                echo "Deep \n";
             }
         }
 
-        $this->ws_client[$interface->resourceId]->send(json_encode($send));
-    }
-
-    public function onMessage(ConnectionInterface $interface, $msg) {
-        //Async Mysql
-        $result = $db_connect->query("SELECT * FROM banner WHERE id = ".$msg." LIMIT 1");
-       
-        $dataUsers = array();
-        $users  = $result->fetchAll();
-        foreach($users as $key => $val) {
-            $dataUsers[$key] = $val;
-        }
-
-        print_r(json_encode($dataUsers)."\n");
-
-        $this->users[$interface->resourceId]->send(json_encode($dataUsers));
     }
 
     public function onClose(ConnectionInterface $interface) {
@@ -104,18 +307,30 @@ class Socket implements MessageComponentInterface {
         $check_user_log = $model->check_userLog($interface->resourceId);
         if(count($check_user_log) > 0) {
             foreach ($check_user_log as $key => $val) {
+
+                $check_on_play = $model->check_on_playing($interface->resourceId, $val->user_token, $val->game_id);
+                if(count($check_on_play) > 0) {
+                    $model->end_outLogWsPlay($interface->resourceId, $val->user_token, $val->game_id);
+                }
+
+
                 $updateLog = $model->update_LogWs($val->id, $interface->resourceId);
                 if($updateLog) {
-                    $send = array(
-                        'code'  => 200,
-                        'datas' => array(),
-                        'msg'   => $val->name.' Leave Game'
-                    );
 
-                    $list_user_in_room = $model->get_roomUsers($val->game_id);
-                    foreach ($list_user_in_room as $keyUsr => $valUsr) {
-                        $this->ws_client[$valUsr->id_ws]->send(json_encode($send)); 
-                    }
+                    foreach ($model->check_user($check_user_log[0]->user_token) as $keyUsrSc => $valUsrSc) {
+                        $list_user_in_room = $model->get_roomData($val->game_id);
+                        foreach ($list_user_in_room as $keyUsr => $valUsr) {
+                            if($valUsr->id_ws != $interface->resourceId) {
+                                $broadcast = array(
+                                    'code'  => 200,
+                                    'datas' => array(),
+                                    'msg'   => $valUsrSc->name.' Leave Game'
+                                );
+
+                                $this->ws_client[$valUsr->id_ws]->send(json_encode($broadcast));                                  
+                            }
+                        }
+                    }                        
                 } else {
                     $send = array(
                         'code'  => 404,
